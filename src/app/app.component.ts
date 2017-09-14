09a022218f5d2b7f4b91a4525bf34369aa97f10f
prashantuppal <prish.uppal@gmail.com>
2017-09-14 15:57:20 -0400

    Simple Invoice Form : Features: 1. Reactive Forms (Angular 4) 2. Dynamic addition of forms 3. functions to add  totals with dynamic forms 4. POST to persist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  invoiceForm: FormGroup;
+
+  post: any;                     // A property for our submitted form
+
+  totalAmountDue:   10;
+  amountDue: number;
+  dueByDate: any;
+  email: string;
+  invoiceName: 'My Invoice';
+  invoiceBillToAddress: '123 main st san jose';
+  invoiceBillToZip: '12345';
+  invoiceNotes: 'notes here';
+
+
+
+
+
+  constructor(private fb: FormBuilder) {
+
+    this.invoiceForm = fb.group({
+      'amountDue' : [null],
+      'dueByDate' : [null],
+      'email' : [null],
+      'invoiceName' : [null],
+
+      'invoiceBillToAddress' : [null],
+      'invoiceBillToZip' : [null],
+      'invoiceNotes' : [null],
+      'totalAmountDue': [null],
+    });
+
+  }
+  addPost(post) {
+    this.email = post.email;
+    this.amountDue = post.amountDue;
+    this.dueByDate = post.dueByDate;
+    this.invoiceName = post.invoiceName;
+    this.invoiceBillToAddress = post.invoiceBillToAddress;
+    this.invoiceBillToZip = post.invoiceBillToZip;
+    this.invoiceNotes = post.invoiceNotes;
+    this.totalAmountDue = post.totalAmountDue;
+    console.log(post);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {  ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { DatepickerExComponent } from './datepicker-ex/datepicker-ex.component';
+import { RouterModule } from '@angular/router';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdDatepickerModule, MdNativeDateModule, MdInputModule} from '@angular/material';
+import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
+import { HttpModule }    from '@angular/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepickerExComponent,
+    InvoiceDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MdDatepickerModule,
+    MdInputModule,
+    MdNativeDateModule,
+    HttpModule,
+     RouterModule.forRoot(
+      [
+        {
+          path: 'invoice-details',
+          component: InvoiceDetailsComponent
+        }
+      ]
+    )
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
